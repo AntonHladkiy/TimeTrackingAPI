11393f8fbfde712d0d8661c4ca0d8d685097e378
AntonHladkiy <gladkyy@ukr.net>
2021-03-19 17:41:15 +0200

    test

--- config/deploy.rb
+++ config/deploy.rb
@@ -76,7 +76,7 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      execute "~/apps/TimeTrackingAPI/current default do bundle exec puma -C /home/deployer/apps/TimeTrackingAPI/shared/puma.rb"
+      execute "cd ~/apps/TimeTrackingAPI/current do bundle exec puma -C /home/deployer/apps/TimeTrackingAPI/shared/puma.rb"
     end
   end
 
